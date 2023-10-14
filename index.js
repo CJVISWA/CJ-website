e60a08754dff683f39b286b49eecbcb652f7830f
CJVISWA <137392054+CJVISWA@users.noreply.github.com>
2023-10-14 16:18:03 +0530

    Version 2

---
+++ index.js
@@ -0,0 +1,19 @@
+function redirect(a)
+{
+    if (a == "education")
+    {
+        window.open("education.html","_blank");
+    }
+    else if (a == "resume")
+    {
+        window.open("resume.html","_blank");
+    }
+    else if (a == "contact")
+    {
+        window.open("contact.html","_blank"); 
+    }
+    else if (a == "home")
+    {
+        window.open("index.html", "_self");
+    }
+}
